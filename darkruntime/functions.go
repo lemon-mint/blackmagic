2c6a1bcfc1097feb6095e6cbdc6dd1179758c59f
lemon-mint <ice.lemons.mint@gmail.com>
2021-10-11 20:26:34 +0900

    GetFunctionByName

--- darkruntime/functions.go
+++ darkruntime/functions.go
@@ -1,9 +1,32 @@
 package darkruntime
 
 import (
+	"fmt"
 	"reflect"
+	"runtime"
 	"unsafe"
 )
+
+func GetFunctionInfo(name string) *runtime.Func {
+	funcs := AllFuncs()
+	for _, fn := range funcs {
+		if fn.Name() == name {
+			return fn
+		}
+	}
+	return nil
+}
+
+var ErrFunctionNotFound = fmt.Errorf("function not found")
+
+func GetFunctionByName(name string, fn interface{}) error {
+	f := GetFunctionInfo(name)
+	if f == nil {
+		return ErrFunctionNotFound
+	}
+	SetFunctionEntry(fn, f.Entry())
+	return nil
+}
 
 func SetFunctionEntry(fn interface{}, entry uintptr) {
 	v := reflect.ValueOf(fn)
